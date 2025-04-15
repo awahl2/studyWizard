b3813c852d199e6daab696dff7e2ccda24979d7e
Anna Wahl <123816763+awahl2@users.noreply.github.com>
2025-04-15 17:43:34 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'assignment_screen.dart'; // Make sure the path is correct
+import 'home_screen.dart';
+import 'pomodoro_screen.dart';
+import 'assignment_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +11,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Assignments App',
+      title: 'Student App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.grey.shade100,
+        fontFamily: 'Arial',
       ),
-      home: AssignmentScreen(),
+      home: MainNavigation(),
     );
   }
 }
+
+class MainNavigation extends StatefulWidget {
+  @override
+  _MainNavigationState createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    HomeScreen(),
+    PomodoroScreen(),
+    AssignmentScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        backgroundColor: Color(0xDDBDAA94),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
