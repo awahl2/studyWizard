d480f59d4aa86b0294ed2923a8331ef427f62414
Anna Wahl <123816763+awahl2@users.noreply.github.com>
2025-04-16 13:51:44 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,68 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'assignment_provider.dart';
+import 'assignment_storage.dart';
 import 'home_screen.dart';
-import 'pomodoro_screen.dart';
 import 'assignment_screen.dart';
+import 'pomodoro_screen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final assignments = await AssignmentStorage.loadAssignments();
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => AssignmentProvider()..setAssignments(assignments),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Student App',
+      title: 'Study Wizard',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey.shade100,
-        fontFamily: 'Arial',
+        primarySwatch: Colors.blue,
       ),
-      home: MainNavigation(),
+      home: const MainNavigation(),
     );
   }
 }
 
 class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
   @override
-  _MainNavigationState createState() => _MainNavigationState();
+  State<MainNavigation> createState() => _MainNavigationState();
 }
 
 class _MainNavigationState extends State<MainNavigation> {
-  int _selectedIndex = 0;
+  int _currentIndex = 0;
 
   final List<Widget> _screens = [
-    HomeScreen(),
-    PomodoroScreen(),
+    const HomeScreen(),
     AssignmentScreen(),
+    const PomodoroScreen(),
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_selectedIndex],
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        backgroundColor: Color(0xDDBDAA94),
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black54,
-        onTap: _onItemTapped,
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
         items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.location_on),
-            label: '',
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.access_time),
-            label: '',
+            icon: Icon(Icons.assignment),
+            label: 'Assignments',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: '',
+            icon: Icon(Icons.timer),
+            label: 'Pomodoro',
           ),
         ],
       ),
