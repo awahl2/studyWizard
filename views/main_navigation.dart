41a1ca2c2c431b3e563ac03d3f1b1f0f68e81e44
Anna Wahl <123816763+awahl2@users.noreply.github.com>
2025-04-22 12:25:41 -0400

    Create main_navigation.dart

---
+++ views/main_navigation.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'assignment_screen.dart';
+import 'home_screen.dart';
+import 'pomodoro_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    HomeScreen(),
+    AssignmentScreen(),
+    PomodoroScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: Colors.deepOrange,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.check_box), label: 'Tasks'),
+          BottomNavigationBarItem(icon: Icon(Icons.timer), label: 'Timer'),
+        ],
+      ),
+    );
+  }
+}
